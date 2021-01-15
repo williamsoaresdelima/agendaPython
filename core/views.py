51e24b6fc59206671616154edfa6b180741c9da6
williamsoaresdelima <43095193+williamsoaresdelima@users.noreply.github.com>
2021-01-15 01:48:11 -0300

    add-insercao-bd

--- core/views.py
+++ core/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from core.models import Evento
-from django.contrib.auth.decorators import  login_required
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-# Create your views here.
 
+
+# Create your views here.
+def evento(request):
+    return render(request, 'evento.html')
 
 def login_user(request):
     return render(request, 'login.html')
@@ -14,11 +17,12 @@ def logout_user(request):
     logout(request)
     return redirect('/')
 
+
 def submit_login(request):
-    if(request.POST):
+    if (request.POST):
         username = request.POST.get('username')
         password = request.POST.get('password')
-        usuario = authenticate(username=username,password=password)
+        usuario = authenticate(username=username, password=password)
         if usuario is not None:
             login(request, usuario)
             return redirect('/')
@@ -27,13 +31,27 @@ def submit_login(request):
     else:
         return redirect('/')
 
+def submit_evento(request):
+    if (request.POST):
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+
+        Evento.objects.create(
+            titulo=titulo,
+            data_evento=data_evento,
+            descricao=descricao,
+            usuario=usuario
+        )
+    return redirect('/agenda')
 
 @login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
     evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos': evento}
-    return render(request,'agenda.html', dados)
+    return render(request, 'agenda.html', dados)
 
 #
 # def index(request):
